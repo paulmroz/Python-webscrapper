0a3014b4aa6b08d2e454b49c3b141c13c3f42686
Pawel <paul@pawel.RF511>
2019-08-22 13:52:56 +0200

    Added simple search

--- movies/views.py
+++ movies/views.py
@@ -13,3 +13,18 @@ def index(request):
     movies = paginator.get_page(page)
 
     return render(request, 'index.html', {'movies': movies})
+
+
+def search(request):
+
+    title = request.GET.get('title')
+
+    movies = Movie.objects.filter(name__icontains=title)
+
+    paginator = Paginator(movies, 15)
+
+    page = request.GET.get('page')
+
+    movies = paginator.get_page(page)
+
+    return render(request, 'index.html', {'movies': movies})
---
+++ venv/lib/python3.6/site-packages/django_elasticsearch_dsl/documents.py
@@ -0,0 +1,168 @@
+from __future__ import unicode_literals
+
+from django.core.paginator import Paginator
+from django.db import models
+from django.utils.six import iteritems
+from elasticsearch.helpers import bulk
+from elasticsearch_dsl import Document as DSLDocument
+
+from .exceptions import ModelFieldNotMappedError
+from .fields import (
+    BooleanField,
+    DateField,
+    DEDField,
+    DoubleField,
+    FileField,
+    IntegerField,
+    KeywordField,
+    LongField,
+    ShortField,
+    TextField,
+)
+from .search import Search
+
+model_field_class_to_field_class = {
+    models.AutoField: IntegerField,
+    models.BigIntegerField: LongField,
+    models.BooleanField: BooleanField,
+    models.CharField: TextField,
+    models.DateField: DateField,
+    models.DateTimeField: DateField,
+    models.EmailField: TextField,
+    models.FileField: FileField,
+    models.FilePathField: KeywordField,
+    models.FloatField: DoubleField,
+    models.ImageField: FileField,
+    models.IntegerField: IntegerField,
+    models.NullBooleanField: BooleanField,
+    models.PositiveIntegerField: IntegerField,
+    models.PositiveSmallIntegerField: ShortField,
+    models.SlugField: KeywordField,
+    models.SmallIntegerField: ShortField,
+    models.TextField: TextField,
+    models.TimeField: LongField,
+    models.URLField: TextField,
+}
+
+
+class DocType(DSLDocument):
+    def __init__(self, related_instance_to_ignore=None, **kwargs):
+        super(DocType, self).__init__(**kwargs)
+        self._related_instance_to_ignore = related_instance_to_ignore
+
+    def __eq__(self, other):
+        return id(self) == id(other)
+
+    def __hash__(self):
+        return id(self)
+
+    @classmethod
+    def search(cls, using=None, index=None):
+        return Search(
+            using=cls._get_using(using),
+            index=cls._default_index(index),
+            doc_type=[cls],
+            model=cls.django.model
+        )
+
+    def get_queryset(self):
+        """
+        Return the queryset that should be indexed by this doc type.
+        """
+        primary_key_field_name = self.django.model._meta.pk.name
+        return self.django.model._default_manager.all().order_by(primary_key_field_name)
+
+    def prepare(self, instance):
+        """
+        Take a model instance, and turn it into a dict that can be serialized
+        based on the fields defined on this DocType subclass
+        """
+        data = {}
+        for name, field in iteritems(self._fields):
+            if not isinstance(field, DEDField):
+                continue
+
+            if field._path == []:
+                field._path = [name]
+
+            prep_func = getattr(self, 'prepare_%s_with_related' % name, None)
+            if prep_func:
+                field_value = prep_func(
+                    instance,
+                    related_to_ignore=self._related_instance_to_ignore
+                )
+            else:
+                prep_func = getattr(self, 'prepare_%s' % name, None)
+                if prep_func:
+                    field_value = prep_func(instance)
+                else:
+                    field_value = field.get_value_from_instance(
+                        instance, self._related_instance_to_ignore
+                    )
+
+            data[name] = field_value
+
+        return data
+
+    @classmethod
+    def to_field(cls, field_name, model_field):
+        """
+        Returns the elasticsearch field instance appropriate for the model
+        field class. This is a good place to hook into if you have more complex
+        model field to ES field logic
+        """
+        try:
+            return model_field_class_to_field_class[
+                model_field.__class__](attr=field_name)
+        except KeyError:
+            raise ModelFieldNotMappedError(
+                "Cannot convert model field {} "
+                "to an Elasticsearch field!".format(field_name)
+            )
+
+    def bulk(self, actions, **kwargs):
+        return bulk(client=self._get_connection(), actions=actions, **kwargs)
+
+    def _prepare_action(self, object_instance, action):
+        return {
+            '_op_type': action,
+            '_index': self._index._name,
+            '_id': object_instance.pk,
+            '_source': (
+                self.prepare(object_instance) if action != 'delete' else None
+            ),
+        }
+
+    def _get_actions(self, object_list, action):
+        if self.django.queryset_pagination is not None:
+            paginator = Paginator(
+                object_list, self.django.queryset_pagination
+            )
+            for page in paginator.page_range:
+                for object_instance in paginator.page(page).object_list:
+                    yield self._prepare_action(object_instance, action)
+        else:
+            for object_instance in object_list:
+                yield self._prepare_action(object_instance, action)
+
+    def update(self, thing, refresh=None, action='index', **kwargs):
+        """
+        Update each document in ES for a model, iterable of models or queryset
+        """
+        if refresh is True or (
+            refresh is None and self.django.auto_refresh
+        ):
+            kwargs['refresh'] = True
+
+        if isinstance(thing, models.Model):
+            object_list = [thing]
+        else:
+            object_list = thing
+
+        return self.bulk(
+            self._get_actions(object_list, action), **kwargs
+        )
+
+
+# Alias of DocType. Need to remove DocType in 7.x
+Document = DocType
